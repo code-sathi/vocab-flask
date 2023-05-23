6d8875bce519b4e9732103060c76b9fca8d403be
Sudip Bhandari <sudip.post@gmail.com>
2023-05-23 19:47:44 +0545

    first commit flask route and nltk

---
+++ hello.py
@@ -0,0 +1,22 @@
+from flask import Flask
+import nltk
+from nltk.corpus import wordnet
+
+app = Flask(__name__)
+nltk.download('wordnet')
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/synonyms/<string:word>")
+def synonyms(word):
+    return "<p>synonyms for " + word + "</p>" + get_synonyms(word).__str__()
+
+
+def get_synonyms(word):
+    synonyms = []
+    for synset in wordnet.synsets(word):
+        for lemma in synset.lemmas():
+            synonyms.append(lemma.name())
+    return synonyms
