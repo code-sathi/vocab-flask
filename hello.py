9978ff3dee93029e7c418819a53f80277f7cb817
Sudip Bhandari <sudip.post@gmail.com>
2023-05-25 06:38:48 +0545

    removed unused imports

--- hello.py
+++ hello.py
@@ -1,10 +1,6 @@
-from flask import Flask, request,jsonify
-import nltk
-from nltk.corpus import wordnet
-from services.hyponyms import replace_with_hyponym
-from services.hypernyms import hypernym_substitute
+from flask import Flask
 from routes.routes import main_routes_bp
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
