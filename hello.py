1655f9fb2af8704106333c87aaa0ae7b5bc958d8
Sudip Bhandari <sudip.post@gmail.com>
2023-05-25 06:05:29 +0545

    enabled cors

--- hello.py
+++ hello.py
@@ -4,8 +4,10 @@ from nltk.corpus import wordnet
 from services.hyponyms import replace_with_hyponym
 from services.hypernyms import hypernym_substitute
 from routes.routes import main_routes_bp
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
 # nltk.download('wordnet')
 # nltk.download('omw-1.4')
 # nltk.download('punkt')
